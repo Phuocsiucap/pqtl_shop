2a63880d2707401d7271872cba6318d85b980e4d
Information-Technology-Project-Development <nguyenvanphuoc09112004@gmail.com>
2025-10-10 23:10:41 +0700

    frist commit

---
+++ BackEnd/pqtl_shop/src/main/java/org/example/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package org.example.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        //dang ky endponit websocket tai //ws/chat
+        //cho phep tat ca cac origin dung(cors) -(co the gioi han neu can)
+        registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins("*");
+
+    }
+}
