84c7bed051081084de4805b65d0cb3f95b0a9aca
Information-Technology-Project-Development <nguyenvanphuoc09112004@gmail.com>
2025-12-15 00:46:25 +0700

    use jdk 17

--- BackEnd/pqtl_shop/src/main/java/org/example/dto/response/UserResponse.java
+++ BackEnd/pqtl_shop/src/main/java/org/example/dto/response/UserResponse.java
@@ -2,6 +2,7 @@ package org.example.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.example.model.Address;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
-
 public class UserResponse {
     private String id;
     private String username;
